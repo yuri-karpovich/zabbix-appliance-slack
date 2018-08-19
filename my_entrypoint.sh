9d50b36525180ef3d87c4c4d96ed28ded01b4238
Yuri Karpovich <spoonest@gmail.com>
2018-08-19 11:11:37 +0300

    some improvements

--- my_entrypoint.sh
+++ my_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm /usr/lib/zabbix/alertscripts/slack.sh
+rm -f /usr/lib/zabbix/alertscripts/slack.sh
 chmod +x /var/lib/zabbix/slack-alert-from-bash-and-zabbix/slack.sh
 mv /var/lib/zabbix/slack-alert-from-bash-and-zabbix/slack.sh /usr/lib/zabbix/alertscripts/slack.sh
 docker-entrypoint.sh
